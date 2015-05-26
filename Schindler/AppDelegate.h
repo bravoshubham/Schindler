e5fbfdf2c1621a5d7a8ce7ae7deac0ec6a45e610
Shubham Srivastava <shubhamsrivastava@Shubhams-Macintosh.local>
2015-05-26 17:58:12 +0530

    Initial Commit

---
+++ Schindler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Schindler
+//
+//  Created by Shubham Srivastava on 26/05/15.
+//  Copyright (c) 2015 Shubham Srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Schindler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Schindler
+//
+//  Created by Shubham Srivastava on 26/05/15.
+//  Copyright (c) 2015 Shubham Srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
